659394b5dec7f5db9e55cecb36622ddf4f3588e6
doraemon <doraemon0916075265@hotmail.com>
2015-06-30 11:38:11 +0800

    bet01

--- TA1/src/T1.java
+++ TA1/src/T1.java
@@ -4,19 +4,23 @@ public class T1 {
 		int p1=0;
 		int p2=0;
 		int countwin=0;
+		int sum=0;
+//		int money=0;
 		
-		for(int k=1;k<=100;k++){
+		for(int k=1;k<=1000;k++){
 			int right=(int) (Math.random()*2+1);
 			System.out.println(right);
 			if(right==1){
 				countwin++;
 				p1++;
 			}else{
+				sum+=countwin;
 				System.out.println(countwin);
 				countwin=0;
 				p2++;
 			}			
 		}
 		System.out.println(p1+"\t"+p2);
+		System.out.println(sum);
 	}
 }
