77120ef37766ff024fa8b5a8ad6f00f2f54de787
doraemon <doraemon0916075265@hotmail.com>
2015-06-30 10:00:04 +0800

    test01
    t1

--- TA1/src/T1.java
+++ TA1/src/T1.java
@@ -1,11 +1,21 @@
 public class T1 {
 
 	public static void main(String[] args) {
-		System.out.println("h1");
-		System.out.println("h2");
-		System.out.println("h3");
-		System.out.println("h4");
-		System.out.println("h5");
-	}
+		int p1=0;
+		int p2=0;
+		
+		for(int k=1;k<=100;k++){
+			int right=(int) (Math.random()*2+1);
+			System.out.println(right);
+			if(right==1){
+				
 
+				p1++;
+			}else{
+				
+				p2++;
+			}			
+		}
+		System.out.println(p1+"\t"+p2);
+	}
 }
