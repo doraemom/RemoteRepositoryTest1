db3f09e1cac6a1ccecc185374839fb623458a355
doraemon <doraemon0916075265@hotmail.com>
2015-06-30 10:17:04 +0800

    測試1

--- TA1/src/T1.java
+++ TA1/src/T1.java
@@ -3,16 +3,17 @@ public class T1 {
 	public static void main(String[] args) {
 		int p1=0;
 		int p2=0;
+		int countwin=0;
 		
 		for(int k=1;k<=100;k++){
 			int right=(int) (Math.random()*2+1);
 			System.out.println(right);
 			if(right==1){
-				
-
+				countwin++;
 				p1++;
 			}else{
-				
+				System.out.println(countwin);
+				countwin=0;
 				p2++;
 			}			
 		}
